68f316e7d3918ae196c536981cec142d30b1aec1
vishal Pawar <133097620+vishalpawar132@users.noreply.github.com>
2023-06-20 17:01:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-function getFormvalue() {
-    //Write your code here
 
+    //Write your code here
+function getFormvalue() {
+    var firstName = document.forms["form1"]["fname"].value;
+    var lastName = document.forms["form1"]["lname"].value;
+    
+    alert("First name: " + firstName + "\nLast name: " + lastName);
 }
+
+
